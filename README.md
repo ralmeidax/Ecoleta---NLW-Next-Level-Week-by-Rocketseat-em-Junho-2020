31c6f7c8524b342bc2a75b99c086143a4d12a588
ralmeidax <ricardo.de.almeidax@gmail.com>
2020-06-08 00:28:56 -0300

    Atualização com Funcionalidades Avançadas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Ecoleta---NLW-Next-Level-Week-by-Rocketseat-em-Junho-2020
-Trilha desenvolvida pela Rocketseat para alavancar conhecimentos em Javascript, Typescript, React, Back-End e Front-End
+# Ecoleta - NLW-Next Level Week 1.0 - by Rocketseat - Junho de 2020
+Trilha desenvolvida pela Rocketseat para alavancar conhecimentos em Javascript, Typescript, React, Back-End e Front-End.
+
+# Imagens do Projeto:
+![alt text](https://raw.githubusercontent.com/ralmeidax/Ecoleta---NLW-Next-Level-Week-by-Rocketseat-em-Junho-2020/master/screens/01-Ecoleta.png)
+![alt text](https://raw.githubusercontent.com/ralmeidax/Ecoleta---NLW-Next-Level-Week-by-Rocketseat-em-Junho-2020/master/screens/02-Ecoleta.png)
+![alt text](https://raw.githubusercontent.com/ralmeidax/Ecoleta---NLW-Next-Level-Week-by-Rocketseat-em-Junho-2020/master/screens/03-Ecoleta.png)
+![alt text](https://raw.githubusercontent.com/ralmeidax/Ecoleta---NLW-Next-Level-Week-by-Rocketseat-em-Junho-2020/master/screens/04-Ecoleta.png)
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import knex from './database/connection';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itensController = new ItemsController();
 
@@ -17,7 +22,25 @@ const itensController = new ItemsController();
 routes.get('/items', itensController.index);
 
 routes.get('/points', pointsController.index);
-routes.post('/points', pointsController.create);
 routes.get('/points/:id', pointsController.show);
+
+routes.post(
+    '/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create);
 
 export default routes;
